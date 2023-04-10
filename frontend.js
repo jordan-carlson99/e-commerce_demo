228ef121bd32c14a8e8bbe237a2978241828dd19
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-04-10 14:27:05 -0500

    hello world front ned server

--- frontend.js
+++ frontend.js
@@ -1,2 +1,17 @@
 import express from "express";
 const app = express();
+import path from "path";
+import dotenv from "dotenv";
+
+let pathToFile = path.resolve("./");
+dotenv.config();
+const port = process.env.frontPort || 3000;
+const url = process.env.frontURL || "127.0.0.1";
+
+app.get("/", (req, res) => {
+  res.sendFile(`${pathToFile}/index.html`);
+});
+
+app.listen(port, url, () => {
+  console.log(`front end running on port ${port} at ${url}`);
+});
