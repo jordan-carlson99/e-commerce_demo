c5b38fdab2429129a298635e89d057bed1f2c489
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-04-10 17:43:18 -0500

    modified patch route to handle if cart goes negative

--- backend.js
+++ backend.js
@@ -64,23 +64,32 @@ app.get("/cart/:user", (req, res) => {
 });
 
 // when client adds or removes a quantity from their cart
-app.patch("/cart/:customer/:add/:product", (req, res) => {
+app.patch("/cart/:customer/:add/:product", async (req, res, next) => {
   let adder = "";
   if (req.params.add == "ADD") {
     adder += "+1";
   } else {
     adder += "-1";
   }
-  client
-    .query(
-      `UPDATE customer_product SET
+  let response = await client.query(
+    `UPDATE customer_product SET
   quantity=quantity${adder} WHERE user_id=$1 AND product_id=$2 RETURNING *`,
-      [req.params.customer, req.params.product]
-    )
-    .then((response) => {
-      res.send("updated");
-    });
+    [req.params.customer, req.params.product]
+  );
+  let data = await response.rows[0].quantity;
+  if (data > 0) {
+    res.send("Modified");
+  } else {
+    // add in handler in app.js for sending a delete when it recieves this
+    res.send("DELETE");
+  }
 });
+
+app.delete("/cart/remove/:customer/:product", (req, res) => {
+  res.send("gucci");
+});
+
+app.delete("/cart/:customer/:product", (req, res) => {});
 
 app.listen(port, url, () => {
   console.log(`backend listening on port ${port} at ${url}`);
