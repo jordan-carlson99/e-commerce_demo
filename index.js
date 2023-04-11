ea8609993831be1cc4ab6aada2e9ea1b9a544d61
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-04-11 12:05:51 -0500

    added flex, function to generate product cards

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log("test 123");
+// change this to env variables when modules aren't maddening
+const apiURL = `http://127.0.0.15:3500`;
+
+function generateMainCard(item) {
+  let card = document.createElement("div");
+  card.className = "card";
+  card.id = item.id;
+  let button = document.createElement("button");
+  button.type = "button";
+  button.className = "card-button";
+  button.innerText = "add to cart";
+  button.addEventListener("click", (e) => {
+    // addToCart(e.target.parentNode)
+    console.log(e.target.parentNode);
+  });
+  card.innerHTML = `
+    <h2 class="card-title">${item.product_name}</h2><br>
+    <img src="" alt="" />picture
+    <h3 class="card-price">${item.price}</h3>`;
+  card.appendChild(button);
+  document.getElementById("card-display-body").appendChild(card);
+  return card;
+}
+
+async function pageLoad() {
+  let data = await getProduct();
+  data.forEach((element) => {
+    generateMainCard(element);
+  });
+}
+
+async function getProduct(id) {
+  id = id || "ALL";
+  return (await fetch(`${apiURL}/products/ALL`)).json();
+}
+/*
+      <div id="card-display-body" class="main">
+        <div class="card">
+          <h2>product name</h2>
+          <h3>price</h3>
+          <img src="" alt="" />picture
+          <button>add to cart</button>
+        </div>
+*/
